d7584e86bca4519d07521c8fe1de5ef837d4ad2e
Manwel Bugeja <manwelbugeja@Manwels-MBP-2.lan>
2022-07-27 16:47:16 +0200

    Updated apk

---
+++ DynamicCode/app/src/main/java/com/example/dynamiccode/UploadContacts.java
@@ -0,0 +1,10 @@
+package com.example.dynamiccode;
+
+import android.util.Log;
+
+public class UploadContacts {
+    public static String TAG = "DYNAMIC_CODE";
+    public void testFn() {
+        Log.i(TAG, "Code successfully running");
+    }
+}
