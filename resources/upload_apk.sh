cd55ac831d9eadd369602b8ecee183ede39f625f
Manwel Bugeja <manwelbugeja@Manwels-MBP-2.lan>
2022-07-27 17:37:02 +0200

    Fixed script

--- resources/upload_apk.sh
+++ resources/upload_apk.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 cp /Users/manwelbugeja/git/android-attacks/DynamicCode/app/build/intermediates/apk/debug/app-debug.apk .
-git add debug-app.apk
+git add app-debug.apk
 git commit -m "Updated apk"
 git push
