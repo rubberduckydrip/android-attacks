e97b5520273ab2281b191517aa6683ce9320b66b
Manwel Bugeja <manwelbugeja@Manwels-MBP-2.lan>
2022-08-21 11:49:42 +0200

    Added benign functionality

--- SMSautomator/app/src/main/java/com/example/smsautomator/MainActivity.java
+++ SMSautomator/app/src/main/java/com/example/smsautomator/MainActivity.java
@@ -1,26 +1,43 @@
 package com.example.smsautomator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static String TAG = "SMS_AUTOMATOR";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Request permission
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.SEND_SMS}, 0);
+        }
     }
 
     public void registerSMS(View view) {
+        // Get Contact
+        EditText text = (EditText)findViewById(R.id.contact);
+        String contact = text.getText().toString();
+
         // Get message from user
-        EditText text = (EditText)findViewById(R.id.message);
+        text = (EditText)findViewById(R.id.message);
         String message = text.getText().toString();
 
         if (message.isEmpty()) {
@@ -28,10 +45,13 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
-        // Get time
+        // Get time and set automator
         text = findViewById(R.id.time);
         int i = Integer.parseInt(text.getText().toString());
         Intent intent = new Intent(this, MyBroadcastReceiver.class);
+        intent.putExtra("contact", contact);
+        intent.putExtra("message", message);
+
         PendingIntent pendingIntent = PendingIntent.getBroadcast(
                 this.getApplicationContext(), 234324243, intent, 0);
         AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
--- SMSautomator/app/src/main/java/com/example/smsautomator/MyBroadcastReceiver.java
+++ SMSautomator/app/src/main/java/com/example/smsautomator/MyBroadcastReceiver.java
@@ -3,6 +3,7 @@ package com.example.smsautomator;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.telephony.SmsManager;
 import android.widget.Toast;
 
 public class MyBroadcastReceiver extends BroadcastReceiver {
@@ -10,5 +11,22 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Toast.makeText(context, "Message will be sent", Toast.LENGTH_LONG).show();
+        String contact = intent.getStringExtra("contact");
+        String message = intent.getStringExtra("message");
+        sendSMS(contact, message, context);
     }
+
+    public void sendSMS(String phoneNo, String msg, Context context) {
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phoneNo, null, msg, null, null);
+            Toast.makeText(context, "Message Sent",
+                    Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            Toast.makeText(context,e.getMessage().toString(),
+                    Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+    }
+
 }
